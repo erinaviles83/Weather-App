5820a72b780a3e58e651972f92db5ac086b877e6
erinaviles83 <140989869+erinaviles83@users.noreply.github.com>
2023-10-01 19:53:46 -0400

    removed week forecast, updated styling

--- script.js
+++ script.js
@@ -1,39 +1,63 @@
-let currentDay = new Date (); 
-
-let p = document.querySelector("p"); 
-
-let hours = currentDay.getHours(); 
-let minutes = currentDay.getMinutes(); 
-
-let days  = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-let day = days[currentDay.getDay()]; 
-
-p.innerHTML = `${day},  ${hours}:${minutes} pm EST`; 
-
-
-function displayWeatherCondition(response) {
-  document.querySelector("#city").innerHTML = response.data.name; 
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
-function search(event) {
-  event.preventDefault(); 
-  let apiKey = "116390a0e4a4a5a1b58dd99c1f83f002";
-  let city = document.querySelector("#new-city").value; 
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-  axios.get(apiUrl).then(displayWeatherCondition); 
-  
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let realFeelElement = document.querySelector("#realfeel");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  realFeelElement.innerHTML = Math.round(response.data.temperature.feels_like);
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-function changeCity(event) {
- event.preventDefault();
- let input = document.querySelector("#new-city");
- console.log(input.value);
-
-  let h1 = document.querySelector("h1");
- h1.innerHTML = `${input.value}`;
+function search(city) {
+  let apiKey = "3a6a306aaebc0ebof78e0326570tda46";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayTemperature);
 }
-let form = document.querySelector("form");
-form.addEventListener("submit", changeCity);
 
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
 
+search("New York");
 
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
